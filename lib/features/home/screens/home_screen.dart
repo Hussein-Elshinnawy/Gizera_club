2c9fe3cbff50194cf3f28062062f636cb6a52ee8
Hussein-Elshinnawy <clorkthefirst@gmail.com>
2024-02-17 16:13:38 +0200

    temp appbar

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -13,6 +13,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60), //default height
+        child: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.transparent,
+                height: 42,
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                  size: 25,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 42,
+                  margin: const EdgeInsets.only(left: 15),
+                  child: Material(
+                    borderRadius: BorderRadius.circular(7),
+                    elevation: 1,
+                    child: TextFormField(
+                      // onFieldSubmitted: navigateToSearchScreen,
+                      decoration: InputDecoration(
+                        prefixIcon: InkWell(
+                          // what does this do
+                          onTap: () {},
+                          child: const Padding(
+                            padding: EdgeInsets.only(left: 6),
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.black,
+                              size: 23,
+                            ),
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.only(top: 10),
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(7),
+                          ),
+                          borderSide: BorderSide.none,
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(7),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.black38,
+                            width: 1,
+                          ),
+                        ),
+                        hintText: 'search in reddit',
+                        hintStyle: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
       backgroundColor: GlobalVariables.greyBackgroundColor,
       body: SafeArea(
         child: SingleChildScrollView(
